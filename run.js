ba5e2fad66e7f52450430cf441d73bd79a0f3968
rlroblesvega <royerleandroroblesvega@gmail.com>
2025-04-05 11:57:09 -0500

    feat: add run.js

--- run.js
+++ run.js
@@ -1,1 +1,21 @@
 console.log("Running!");
+
+
+const { exec } = require("child_process");
+const path = require("path");
+
+
+const scriptPath = path.join(__dirname, "script.js");
+const command = `node ${scriptPath}`;
+
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error executing script: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Script error: ${stderr}`);
+    return;
+  }
+  console.log(`Script output: ${stdout}`);
+})
